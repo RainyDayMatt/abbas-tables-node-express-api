9a6787d4740581c483a36f6b8cee7ed37f6d7129
Matt Harp <matthewharp@gmail.com>
2019-03-18 12:57:05 -0500

    Added CORS support through middleware.

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,10 +1,12 @@
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const expressValidator = require("express-validator");
 
 module.exports = {
     init(app) {
         app.use(bodyParser.urlencoded({extended: true}));
         app.use(bodyParser.json());
+        app.use(cors());
         app.use(expressValidator());
     }
 };
