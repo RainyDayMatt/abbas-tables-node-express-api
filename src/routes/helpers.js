3480a28276ccda5eb32601211b94937adae56f78
Matt Harp <matthewharp@gmail.com>
2020-01-03 15:49:17 -0600

    wi

--- src/routes/helpers.js
+++ src/routes/helpers.js
@@ -4,7 +4,7 @@ module.exports = {
     checkUserEmailAvailability(req, res, next) {
         userQueries.checkUserEmailAvailability(req.body.email, (message) => {
             if (message) {
-                return res.status(400).json({ errors: [ message ] });
+                return res.status(400).json({ err: [message] });
             }
             else {
                 return next();
